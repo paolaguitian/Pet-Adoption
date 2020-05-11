24cee5dd97b9ed54ea69fa3bcd8f78bc8209d9e7
Paola Guitian <poliguitian@gmail.com>
2020-05-11 16:31:19 -0400

    async and destructuring

--- src/searchParams.js
+++ src/searchParams.js
@@ -1,27 +1,52 @@
 import React, { useEffect, useState } from "react";
 import pet, { ANIMALS } from '@frontendmasters/pet'
 import useDropdown from './useDropdown';
+import Results from "./Results";
 
 const SearchParams = () => {
   const [breeds, updateBreeds] = useState([])
   const [AnimalDropdown, animal] = useDropdown("Animal", "dog", ANIMALS);
   const [BreedDropdown, breed, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+
+  async function requestPets() {
+    //{animals, pagination}
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    console.log("animals", animals);
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
-    updateBreeds([]);
     setBreed("");
 
-    //apuBreeds:{breeds: [{name,link}, {}]}
+    //apiBreeds:{breeds: [{name,link}, {}]}
     pet.breeds(animal).then(({ breeds: apiBreeds }) => {
       const breedString = apiBreeds.map(({ name }) => name);
       updateBreeds(breedString);
     }, console.error)
   }, [animal, updateBreeds, setBreed])
 
+
   return (
     <div>
-      <AnimalDropdown />
-      <BreedDropdown />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
     </div>
   );
 };

b032c136b77f06b422751ea840068bdae9d6de33
Paola Guitian <poliguitian@gmail.com>
2020-05-12 12:33:26 -0400

    carousel bug fix

--- src/carousel.js
+++ src/carousel.js
@@ -1,50 +1,44 @@
-import React from 'react';
+import React from "react";
 
 class Carousel extends React.Component {
   state = {
-    active: 0,
     photos: [],
-  }
-
+    active: 0
+  };
   static getDerivedStateFromProps({ media }) {
-    let photos = ['http://placecorgi.com/600/600'];
+    let photos = ["http://placecorgi.com/600/600"];
 
     if (media.length) {
       photos = media.map(({ large }) => large);
     }
 
-    return { photos }
+    return { photos };
   }
-
   handleIndexClick = event => {
     this.setState({
-      active: +event.dataset.index,
+      active: +event.target.dataset.index
     });
-  }
-
+  };
   render() {
     const { photos, active } = this.state;
-
     return (
       <div className="carousel">
         <img src={photos[active]} alt="animal" />
         <div className="carousel-smaller">
-          {
-            this.props.media.map((photo, index) => {
-              <img
-                key={photo.large}
-                onClick={this.handleIndexClick}
-                onKeyUp={this.handleIndexClick}
-                data-index={index}
-                src={photo.large}
-                className={index === active ? 'active' : ""}
-                alt="animal thumbnail"
-              />
-            })
-          }
+          {photos.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
         </div>
       </div>
-    )
+    );
   }
 }
 
